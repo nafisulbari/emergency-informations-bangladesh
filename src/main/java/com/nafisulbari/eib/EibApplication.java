d9a939ffb7eb5c910aae14f50d2ef2875981a64c
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-02-04 03:43:02 +0600

    boot launched

---
+++ src/main/java/com/nafisulbari/eib/EibApplication.java
@@ -0,0 +1,13 @@
+package com.nafisulbari.eib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EibApplication.class, args);
+    }
+
+}
