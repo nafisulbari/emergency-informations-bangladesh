dc6a036f1b5680c7520ea9de4ffc70f676b619a6
Ahmed Nafisul Bari <nafisulbari@gmail.com>
2020-02-14 23:38:36 +0600

    Housekeeping

--- src/main/java/com/nafisulbari/eib/controller/LoginController.java
+++ src/main/java/com/nafisulbari/eib/controller/LoginController.java
@@ -1,25 +1,15 @@
 package com.nafisulbari.eib.controller;
 
-import com.nafisulbari.eib.model.Citizen;
-import com.nafisulbari.eib.model.Hospital;
-import com.nafisulbari.eib.model.MedicalRecord;
-import com.nafisulbari.eib.model.User;
 import com.nafisulbari.eib.service.CitizenService;
 import com.nafisulbari.eib.service.HospitalService;
 import com.nafisulbari.eib.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.AuthenticatedPrincipal;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
 
 
 @Controller
